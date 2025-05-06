8de4afc4e50aa40ccaf260d70ffd28036bfe5138
Axel Westman <111924075+AxelWestman@users.noreply.github.com>
2025-05-06 10:11:44 -0300

    agregando rutas

--- src/controllers/ejercicios_y_rutinas.js
+++ src/controllers/ejercicios_y_rutinas.js
@@ -2,8 +2,70 @@ const conectsql = require("../config/configsql");
 
 const controller = {
 
+    /*********************
+  * OBTENER EJERCICIOS *
+  *********************/
+
+    getEjercicios: async (req, res) => {
+      let connection;
+  
+      try {
+        connection = await conectsql();
+        const [rows] = await connection.execute("SELECT * FROM ejercicios");
+        console.log(rows);
+  
+        res.json({
+          status: "success",
+          data: rows,
+        });
+      } catch (error) {
+        console.log("Error al obtener ejercicios");
+        res.status(500).json({
+          status: "error",
+          message: "Error al obtener ejercicios" + error,
+          error,
+        });
+      } finally {
+        if (connection) {
+          connection.close();
+          console.log("Conexión cerrada");
+        }
+      }
+    },
+
+/*******************
+ * OBTENER RUTINAS *
+ *******************/
+
+getRutinas: async (req, res) => {
+  let connection;
+
+  try {
+    connection = await conectsql();
+    const [rows] = await connection.execute("SELECT * FROM rutinas");
+    console.log(rows);
+
+    res.json({
+      status: "success",
+      data: rows,
+    });
+  } catch (error) {
+    console.log("Error al obtener rutinas");
+    res.status(500).json({
+      status: "error",
+      message: "Error al obtener rutinas" + error,
+      error,
+    });
+  } finally {
+    if (connection) {
+      connection.close();
+      console.log("Conexión cerrada");
+    }
+  }
+},
+
      /*********************
-  * AGREGAAR EJERCICIOS *
+  * AGREGAR EJERCICIOS *
   *********************/
 
   postEjercicios: async (req, res) => {
@@ -40,9 +102,46 @@ catch (error) {
       console.log("Conexión cerrada");
     }
   }
+},
+
+/*******************
+ * AGREGAR RUTINAS *
+ *******************/
+postRutinas: async (req, res) => {
+  console.log(req.body);
+  const {nombre_rutina, descripcion, duracion_semanas, nivel_dificultad} = req.body;
+  let connection;
+
+  try{
+    connection = await conectsql();
+    const [result] = await connection.execute(
+      "INSERT INTO rutinas (nombre_rutina, descripcion, duracion_semanas, nivel_dificultad) VALUES (?, ?, ?, ?)",
+      [nombre_rutina, descripcion ? descripcion : null, duracion_semanas ? duracion_semanas : null, nivel_dificultad ? nivel_dificultad : null]
+    );
+    console.log(result);
+
+    res.json({
+      status: "success",
+      data: result,
+    });
+  } catch(error){
+    console.log("Error al agregar rutina");
+    res.status(500).json({
+      status: "error",
+      message: "Error al agregar rutina" + error,
+      error,
+    });
+}
+finally {
+  if (connection) {
+    connection.close();
+    console.log("Conexión cerrada");
+  }
 }
 
-};
+}
+
+}
 
 module.exports = controller;
 
--- src/routes/ejercicios_y_rutinas.js
+++ src/routes/ejercicios_y_rutinas.js
@@ -3,9 +3,17 @@ var EjerciciosYrutinasController = require('../controllers/ejercicios_y_rutinas'
 var router = express.Router();
 
 /**************
+ * RUTAS GET *
+ **************/
+
+router.get('/getEjercicios', EjerciciosYrutinasController.getEjercicios);
+router.get('/getRutinas', EjerciciosYrutinasController.getRutinas);
+
+/**************
  * RUTAS POST *
  **************/
 
-router.post('/postEjercicios', EjerciciosYrutinasController.postEjercicios)
+router.post('/postEjercicios', EjerciciosYrutinasController.postEjercicios);
+router.post('/postRutinas', EjerciciosYrutinasController.postRutinas);
 
 module.exports = router;
